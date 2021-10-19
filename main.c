04d8663ecb932a896b33ea51024dd78087e832dd
Viviana Muñoz <vsmunoz@espol.edu.ec>
2021-10-19 15:54:25 -0500

    final

--- main.c
+++ main.c
@@ -26,39 +26,50 @@ int main(int argc, char **argv){
 	//Puede declarar más variables si lo necesita.
 	
 	 
-	char peso[100]={0};
-	char altura[100]={0};
-	if (argc==0){
-	printf("Número de personas invalido\n");
-	
+	float peso[100];
+	float altura[100];
+	if (elementos<=0){
+	printf("Número invalido\n");
+	exit(1);
 	}
 		int i=0;
 	while(i<elementos){
 		printf("Peso: ");
-		fgets(peso,10,stdin);
-		float valor=atof(peso);
+		float entrada1;
+		getc(stdin);
+		scanf("%.2f",&entrada1);
+		getc(stdin);
+		peso[i]=entrada1;
+		float entrada2;
 		printf("Altura: ");
-		fgets(altura,10,stdin);
+		getc(stdin);
+		scanf("%.2f",&entrada2);
+		getc(stdin);
+		altura[i]=entrada2;
 		i++;
-	};
+		getc(stdin);
+		if (entrada1<=0||entrada2<=0){printf("Numero de personas invalido");
+exit(1);	}}
 	
 	float sum = 0.0f;
 	float max_imc = 0.0f;
 	float imcAll[elementos];
-	for(int i;i<elementos;i++){
-		float valor1=atof(peso[i]);
-		float valor2=atof(altura[i]);
+
+	for(int i=0;i<elementos;i++){
+		float valor1=peso[i];
+		float valor2=altura[i];
 		float imc=valor1/(valor2*valor2);
+		imcAll[i]=imc;
 	}
-	float nma=0.0;
-	 for(int i;i<elementos;i++){
+	float nma=0.0f;
+	 for(int i=0;i<elementos;i++){
 	 sum+=imcAll[i];
-	if(imcAll[i]>nma)
-	nma=imcAll[i];
-	  };
-	  
-	  sum = sum/elementos;
-	
+	 if(imcAll[i]>nma){
+	 nma=imcAll[i];
+	}
+	 }
+	 float operacion=(sum/elementos);
+        sum = operacion;
 	max_imc=nma;
 	//No modifique estas lineas
 	//Guarde los resultados en las 
